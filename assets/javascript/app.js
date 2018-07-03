1cd4128e81ad5ee3ff4b1531a8254e7770aa1701
Daemone <kylebehm@gmail.com>
2018-07-03 17:20:24 -0400

    logic_working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,44 @@ var correctAnswers = 0;
 
 var incorrectAnswers = 0;
 
-var unansweredQuestions = 4;
+var unansweredQuestions = 0;
 
 var answer = ["a", "b", "c", "d"];
+
+var userAnswer = ["", "", "", ""];
+
+// Function to calculate results
+function resultsCalculator() {
+    // get userAnswer from buttons
+    var userAnswerOne = $("input[name='firstQuestionAnswers']:checked").val();
+    console.log(userAnswerOne);
+    userAnswer.splice(0,1,userAnswerOne);
+    console.log(userAnswer);
+
+    var userAnswerTwo = $("input[name='secondQuestionAnswers']:checked").val();
+    userAnswer.splice(1,1,userAnswerTwo);
+
+    var userAnswerThree = $("input[name='thirdQuestionAnswers']:checked").val();
+    userAnswer.splice(2,1,userAnswerThree);
+
+    var userAnswerFour = $("input[name='fourthQuestionAnswers']:checked").val();
+    userAnswer.splice(3,1,userAnswerFour);
+
+    // tally up totals
+    for (i=0; i < answer.length; i++) {
+        if (userAnswer[i] == "") {
+            unansweredQuestions++;
+        }
+        else if (userAnswer[i] == answer[i]) {
+            correctAnswers++;
+        }
+        else {
+            incorrectAnswers++ 
+        } 
+    }
+}
+
+
 
 $(document).ready(function () {
     // startButton jquery function to display questions 
@@ -23,66 +58,14 @@ $(document).ready(function () {
         $("p").show();
         $("#results").hide();
         $(".form-check").show();
+        $("#submitButton").show();
+    });
+
+    // submitButton jquery function to display results 
+    $("#submitButton").on("click", function () {
+        gameOver();
     });
 });
-
-// function resultsCalculator() {
-//     $(document).ready(function () {
-//         var firstQuestion = $("input[name='firstQuestionAnswers']:checked").val();
-//         if (firstQuestion !==1 || 2 || 3 || 4) {
-//             return
-//         }
-//         else if (firstQuestion == answer[0]) {
-//             correctAnswers++;
-//             unansweredQuestions--;
-//         }
-//         else {
-//             incorrectAnswer--;
-//             unansweredQuestions--;
-//         }
-
-//         var secondQuestion = $("input[name='secondQuestionAnswers']:checked").val();
-//         if (secondQuestion !==1 || 2 || 3 || 4) {
-//             return
-//         }
-//         else if (secondQuestion == answer[1]) {
-//             correctAnswers++;
-//             unansweredQuestions--;
-//         }
-//         else {
-//             incorrectAnswer--;
-//             unansweredQuestions--;
-//         }
-
-//         var thirdQuestion = $("input[name='thirdQuestionAnswers']:checked").val();
-//         if (thirdQuestion !==1 || 2 || 3 || 4) {
-//             return
-//         }
-//         else if (thirdQuestion == answer[2]) {
-//             correctAnswers++;
-//             unansweredQuestions--;
-//         }
-//         else {
-//             incorrectAnswer--;
-//             unansweredQuestions--;
-//         }
-
-//         var fourthQuestion = $("input[name='fourthQuestionAnswers']:checked").val();
-//         if (fourthQuestion !==1 || 2 || 3 || 4) {
-//             return
-//         }
-//         else if (fourthQuestion == answer[3]) {
-//             correctAnswers++;
-//             unansweredQuestions--;
-//         }
-//         else {
-//             incorrectAnswer--;
-//             unansweredQuestions--;
-//         }
-//     });
-// }
-
-
 
 
 // function to transition to results page
@@ -90,23 +73,27 @@ function gameOver() {
     // resultsCalculator();
     clearInterval(intervalId);
     time = 9;
+    resultsCalculator();
     displayText();
     console.log("reset");
     $("h2").hide();
     $("p").hide();
     $(".form-check").hide()
+    $("#submitButton").hide();
     $("#results").show();
     $("#resetButton").show();
-
+    
     // onclick function to reset game
     $(document).ready(function () {
-
+        
         // startButton jquery function to display intro 
         $("#resetButton").on("click", function () {
             console.log("reset");
             $("#startButton").show();
             $("#results").hide();
             $("#resetButton").hide();
+            // uncheck radio buttons
+            $("input[type=radio]").prop("checked", false) ;
         });
     });
 }
@@ -125,5 +112,5 @@ function timer() {
     $("#timer").html(time);
     if (time == 0) {
         gameOver();
-    } 
+    }
 }
